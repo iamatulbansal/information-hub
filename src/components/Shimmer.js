847467a0160d95a32807b72e7595a551aab3f121
ATUL BANSAL <86524139+iamatulbansal@users.noreply.github.com>
2023-05-19 17:24:00 +0530

    added lazy loading and shimmer

---
+++ src/components/Shimmer.js
@@ -0,0 +1,6 @@
+const Shimmer = () => {
+    return <div className='Shimmer'>
+        <div className="img-shimmer"> <img width={"700px"} src="https://raw.githubusercontent.com/markiv/SwiftUI-Shimmer/main/docs/loading.gif?raw=true" /></div>
+    </div>
+}
+export default Shimmer;
--- src/route/appRoute.js
+++ src/route/appRoute.js
@@ -1,16 +1,30 @@
-import { createBrowserRouter } from "react-router-dom";
+import { lazy, Suspense } from "react";
 import { ROUTE_ROOT_PATH } from "../constants/constant";
+import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Body from "../components/Body";
+import Shimmer from '../components/Shimmer'
 import Error from "../pages/Error";
+import { mainData } from "../utils/helper";
+
 //Pages
-import CssQuestion from "../pages/CssQuestion";
-import HtmlQuestion from "../pages/HtmlQuestion";
-import JavascriptQuestion from "../pages/JavascriptQuestion";
-import NodeQuestion from "../pages/NodeQuestion";
-import ReactQuestion from "../pages/ReactQuestion";
-import SecurityQuestion from "../pages/SecurityQuestion";
-import { mainData } from '../utils/helper'
+// import CssQuestion from "../pages/CssQuestion";
+// import HtmlQuestion from "../pages/HtmlQuestion";
+// import JavascriptQuestion from "../pages/JavascriptQuestion";
+// import NodeQuestion from "../pages/NodeQuestion";
+// import ReactQuestion from "../pages/ReactQuestion";
+// import SecurityQuestion from "../pages/SecurityQuestion";
+
+//Lazy loading pages
+const CssQuestion = lazy(() => import("../pages/CssQuestion"));
+const HtmlQuestion = lazy(() => import("../pages/HtmlQuestion"));
+const JavascriptQuestion = lazy(() => import("../pages/JavascriptQuestion"));
+const NodeQuestion = lazy(() => import("../pages/NodeQuestion"));
+const ReactQuestion = lazy(() => import("../pages/ReactQuestion"));
+const SecurityQuestion = lazy(() => import("../pages/SecurityQuestion"));
+
+
+
 
 
 
@@ -27,27 +41,49 @@ const appRoute = createBrowserRouter([
             },
             {
                 path: ROUTE_ROOT_PATH.CSS,
-                element: <CssQuestion {...mainData} />,
+                element: (
+                    <Suspense fallback={<Shimmer />}>
+                        <CssQuestion {...mainData} />,
+                    </Suspense>
+                ),
             },
             {
                 path: ROUTE_ROOT_PATH.HTML,
-                element: <HtmlQuestion {...mainData} />,
+                element: (
+                    <Suspense fallback={<Shimmer />}>
+                        <HtmlQuestion {...mainData} />,
+                    </Suspense>
+                ),
             },
             {
                 path: ROUTE_ROOT_PATH.JAVASCRIPT,
-                element: <JavascriptQuestion {...mainData} />,
+                element: (
+                    <Suspense fallback={<Shimmer />}>
+                        <JavascriptQuestion {...mainData} />,
+                    </Suspense>
+                ),
             },
             {
                 path: ROUTE_ROOT_PATH.NODE,
-                element: <NodeQuestion {...mainData} />,
+                element: <Suspense fallback={<Shimmer />}>
+                    <NodeQuestion {...mainData} />,
+                </Suspense>
             },
             {
                 path: ROUTE_ROOT_PATH.REACT,
-                element: <ReactQuestion {...mainData} />,
+                element: (
+                    <Suspense fallback={<Shimmer />}>
+                        <ReactQuestion {...mainData} />,
+                    </Suspense>
+                ),
             },
             {
                 path: ROUTE_ROOT_PATH.SECURITY,
-                element: <SecurityQuestion {...mainData} />,
+                element: (
+                    <Suspense fallback={<Shimmer />}>
+                        <SecurityQuestion {...mainData} />,
+                    </Suspense>
+                ),
             },
         ],
     },
